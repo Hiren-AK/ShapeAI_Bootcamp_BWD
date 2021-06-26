82a09acc4b87ee909e79c2b8248306009f5d4da6
Hiren Arvind Kachhadia <hirenarvind2013@gmail.com>
2021-06-26 15:56:59 +0400

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Dev. with Javascript and React.js</h1>
+      <p>
+        A bootcamp that introduces Javascript, React.js, HTML and basic web
+        design from scratch.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
